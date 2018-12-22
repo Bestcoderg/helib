3430e1a55d68ca8c01454b4cbf2587132fe62faf
bestcoderg <624303639@qq.com>
2018-12-22 10:21:15 +0800

    将二进制的比较模块换成一个整数的比较模块

--- sort_new.cpp
+++ sort_new.cpp
@@ -15,6 +15,7 @@
 #include <cmath>
 #include <algorithm>
 #include <NTL/BasicThreadPool.h>
+#include <time.h>
 NTL_CLIENT
 
 #include "EncryptedArray.h"
@@ -23,8 +24,14 @@ NTL_CLIENT
 #include "intraSlot.h"
 #include "binaryArith.h"
 #include "binaryCompare.h"
+
+#include "GreaterThan.hpp"
+#include "Timer.hpp"
+#include<bits/stdc++.h>
+
+const int MAX_N = 33;
 int N,T,S;
-int a[10]; // 明文数组
+long a[MAX_N]; // 明文数组
 
 Ctxt Equal(std::vector<Ctxt> m1, std::vector<Ctxt> m2);
 Ctxt Equal2(std::vector<Ctxt> m1, std::vector<Ctxt> m2);
@@ -32,26 +39,40 @@ Ctxt mulOrdiVector(vector<Ctxt>& vArray,EncryptedArray& ea,long nslots);
 Ctxt divOrMul(std::vector<Ctxt> array);
 Ctxt Less(std::vector<Ctxt> m1, std::vector<Ctxt> m2);
 Ctxt divOrAdd(std::vector<Ctxt> array);
+void full_adder(std::vector<Ctxt>& val , Ctxt x);
 
 int main(int argc, char *argv[])
 {
-    long m = 0;                   // Specific modulus
-	long p = 2;                   // Plaintext base [default=2], should be a prime number
+    //freopen("in.txt" , "r" , stdin);
+    //freopen("out.txt" , "w" , stdout);
+
+    ArgMapping amap;
+	long m = 8192;
+	long p = 113; // small domain
+    long L = 4;
+    amap.arg("m", m, "m");
+    amap.arg("p", p, "p");
+    amap.arg("L", L, "L");
+    amap.parse(argc, argv);
+
+
+    /*long m = 8192;                   // Specific modulus
+	long p = 113;                   // Plaintext base [default=2], should be a prime number
 	long r = 1;                   // Lifting [default=1]
 	long L = 16;                  // Number of levels in the modulus chain [default=heuristic]
 	long c = 2;                   // Number of columns in key-switching matrix [default=2]
 	long w = 64;                  // Hamming weight of secret key
 	long d = 0;                   // Degree of the field extension [default=1]
 	long k = 80;                 // Security parameter [default=80] 
-    long s = 0;                   // Minimum number of slots [default=0]
+    long s = 0;                   // Minimum number of slots [default=0]*/
 
-	std::cout << "Finding m... " << std::flush;
-	m = FindM(k, L, c, p, d, s, 0);                            // Find a value for m given the specified values
+	//std::cout << "Finding m... " << std::flush;
+	//m = FindM(k, L, c, p, d, s, 0);                            // Find a value for m given the specified values
 	std::cout << "m = " << m << std::endl;
 	
 	std::cout << "Initializing context... " << std::flush;
-	FHEcontext context(m, p, r); 	                        // Initialize context
-	buildModChain(context, L, c);                           // Modify the context, adding primes to the modulus chain
+	FHEcontext context(m, p, 1); 	                        // Initialize context
+	buildModChain(context, L);                           // Modify the context, adding primes to the modulus chain
 	std::cout << "OK!" << std::endl;
 
 	std::cout << "Creating polynomial... " << std::flush;
@@ -61,35 +82,220 @@ int main(int argc, char *argv[])
 	std::cout << "Generating keys... " << std::flush;
 	FHESecKey secretKey(context);                           // Construct a secret key structure
 	const FHEPubKey& publicKey = secretKey;                 // An "upcast": FHESecKey is a subclass of FHEPubKey
-	secretKey.GenSecKey(w);                                 // Actually generate a secret key with Hamming weight w
+	secretKey.GenSecKey(64);                                 // Actually generate a secret key with Hamming weight w
+    
+	setup_auxiliary_for_greater_than(&secretKey);
+    auto gt_args = create_greater_than_args(0, 1, context);
+
     //add1DMatrices(secretKey);
 	std::cout << "OK!" << std::endl;
 	/*** END INITIALIZATION ***/  
 
-    /*printf("please input the number of your vec:");
+    
+    printf("please input the number of your vec:");
     scanf("%d",&N);
     for(int i=0;i<N;i++){
-        scanf("%d",&a[i]);
-    }*/
-    /*** END INPUT ***/
-    /*NTL::Vec<Ctxt> enca[10],Y[10]; // 加密后的数组
+        scanf("%ld",&a[i]);
+    }
+
+
+    /*************** time **********************/
+    clock_t start, finish;
+    start = clock();
+    NTL::Vec<Ctxt> enc;
+    
+    //std::vector<Ctxt> enc;
+    //std::vector<Ctxt> M[MAX_N];
+    NTL::Vec<Ctxt> M[MAX_N];
+   
+    Ctxt my_one(publicKey);
+    publicKey.Encrypt(my_one,ZZX(1));
+    Ctxt my_zero(publicKey);
+    publicKey.Encrypt(my_zero,ZZX(0));
+
+    resize(enc, N, my_zero);
+    for(int i = 0; i< N; i++) {
+        resize(M[i], N, my_zero);
+    }
+    
+
     for(int i=0;i<N;i++){
-        for(int j =0;j<32 ;j++){
-            publicKey.Encrypt(enca[i][j] , ZZX(0));
-            publicKey.Encrypt(Y[i][j] , ZZX(0));
-            enca[i][j].modDownToLevel(5);
-            Y[i][j].modDownToLevel(5);
+        for(int j=0;j<N;j++){
+            //M[i].push_back(my_zero);
+            M[i][j] = my_zero;
         }
-    }*/
+    }
+    finish = clock();
+    std::cout<<"初始化M完成:"<<(double)(finish-start)/CLOCKS_PER_SEC<<endl;
+    start = clock();
+    for(int i=0;i<N;i++){
+        Ctxt t = encrypt_in_degree(a[i], publicKey);
+        //enc.push_back(t);
+        enc[i] = t;
+    }
+    finish = clock();
+    std::cout<<"加密完成："<<(double)(finish-start)/CLOCKS_PER_SEC<<endl;
+    cout << "___________________加密完成________________" << endl;
+
+    for(int i=0;i<N;i++){
+
+        ZZX ptResult;
+        secretKey.Decrypt(ptResult, enc[i]);
+        std::cout<<"!!!!!!!!!!!!!!!!!!!!!"<<ptResult[0];
+        std::cout << std::endl;
+    }
+
+
+
+
+
+
+    start = clock();
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            auto tmp = greater_than(enc[i], enc[j], context);
+            M[i][j] += tmp;
+            auto tmpp = greater_than(enc[j], enc[i], context);
+            M[j][i] =tmpp;
+
+
+
+
+            // M[j][i] += tmp;
+            // M[j][i].addCtxt(my_one);
+            
+        }
+    }
+
+    for(int i=0;i<N;i++){
+
+        ZZX ptResult;
+        secretKey.Decrypt(ptResult, enc[i]);
+        std::cout<<"!!!!!!!!!!!!!!!!!!!!!"<<ptResult[0];
+        std::cout << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    finish = clock();
+    std::cout<<"M赋值："<<(double)(finish-start)/CLOCKS_PER_SEC<<std::endl;
+     for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            
+            ZZX ptResult;
+            secretKey.Decrypt(ptResult, M[i][j]);
+            std::cout<<ptResult[0];
+        }
+        cout<<endl;
+    }
+     cout << "___________________M完成________________" << endl;
+
+    /***********sort***************/
+    
+    //std::vector<Ctxt> val; //val[i]-第i个元素在排序后序列的位置,一共分为4位。
+    NTL::Vec<Ctxt> val;
+    resize(val ,N, my_zero);
+    // for(int i=0;i<N;i++){
+    //     val.push_back(my_zero);
+        
+    // }
+    start = clock();
+    for(int i=0;i<N;i++){
+        Ctxt tmp(publicKey);
+        tmp = my_zero;
+        for(int j =0;j<N;j++){
+            tmp += M[j][i];
+            //full_adder(val[i] , M[j][i]);
+        }
+        //val.push_back(tmp);
+        val[i] = tmp;
+    }
+    finish = clock();
+    std::cout<<"汉明权重："<<(double)(finish-start)/CLOCKS_PER_SEC<<std::endl;
+    cout << "___________________汉明权重完成________________" << endl;
+    
+    for(int i=0;i<N;i++){
+
+        ZZX ptResult;
+        secretKey.Decrypt(ptResult, val[i]);
+        std::cout<<ptResult[0];
+        std::cout << std::endl;
+    }
+    start = clock();
+    cout << "___________________输出完成________________" << endl;
+
+
+
+
+
+
+
+    //std::vector<Ctxt> tta;
+    NTL::Vec<Ctxt> tta;
+    resize(tta, N, my_zero);
+    for(long i=0;i<N;i++){
+        Ctxt t(publicKey);
+        t = encrypt_in_degree(i, publicKey);
+        //tta.push_back(t);
+        tta[i] = t;
+
+        Ctxt ans(publicKey);
+        ans = my_zero;
+        for(int j=0;j<N;j++){
+
+            // Ctxt tmp = greater_than(tta[i], val[j], context);
+            // Ctxt tmpp = greater_than(val[j], tta[i], context);
+            // tmp.multiplyBy(tmpp);
+            // tmp.multiplyBy(enc[j]);
+            // ans += tmp;
+
+            ZZX ptResult,abccd;
+            secretKey.Decrypt(ptResult,tta[i]);
+            secretKey.Decrypt(abccd,val[j]);
+            std::cout<<ptResult[0] << "......"<<abccd[0]<<endl;
+
+        }
+        cout << "______________________"<<endl;
+        // ZZX ptResult;
+        // secretKey.Decrypt(ptResult,ans);
+        // std::cout<<ptResult[0] << endl;
+
+    }
+    finish = clock();
+    std::cout<<"排序:"<<(double)(finish-start)/CLOCKS_PER_SEC<<std::endl;
+    cout << "___________________排序完成________________" << endl;
+    
     //EncryptedArray ea(context, G);
     //long nSlots = ea.size();
-
+    /*
     Ctxt result(publicKey);
     std::vector<Ctxt> enca, encb;
-    for(int i=0;i<8;i++){
+    for(int i=7;i>=0;i--){
         Ctxt c1(publicKey), c2(publicKey);
-        publicKey.Encrypt(c1, ZZX((20>>i)&1));
-        publicKey.Encrypt(c2, ZZX((23>>i)&1));
+        publicKey.Encrypt(c1, ZZX((120>>i)&1));
+        publicKey.Encrypt(c2, ZZX((123>>i)&1));
         enca.push_back(c1);
         encb.push_back(c2);
     }
@@ -97,47 +303,22 @@ int main(int argc, char *argv[])
     ZZX ptResult;
     secretKey.Decrypt(ptResult, result);
     cout<<ptResult[0]<<endl;
-    /*** END ENCRYPT ***/
-    /*NTL::Vec<Ctxt> M[5],S;
-    Ctxt zeroCtxt(publicKey);
-    publicKey.Encrypt(zeroCtxt,ZZX(0));
-    for(int i=0;i<5;i++)
-        resize(M[i], 5 , zeroCtxt);
-    resize(S,5,zeroCtxt);*/
-
-    /*Ctxt e(publicKey),g(publicKey);
-    for(int i=0;i<5;i++){
-    for(int j=i+1;j<5;j++){
-        compEqGt(CtPtrs_VecCt(e),CtPtrs_VecCt(g),CtPtrs_VecCt(enca[i]),CtPtrs_VecCt(enca[j]));
-        M[j][i] = g;
-        g.addConst(to_ZZX(1));
-        M[i][j] = g;
-    }
-    for(int i=0;i<5;i++){
-      for(int j=0;j<5;j++){
-        S[i].addCtxt(M[j][i]);
-      }
-    }
-    Ctxt tmp(publicKey);
-    publicKey.Encrypt(tmp,ZZX(0));
     */
+    /*** END ENCRYPT ***/
+   
 
-    /*for(int i=0;i<5;i++,tmp.addConst(to_ZZX(1))){
-      for(int j=0;j<5;j++){
-        compEqGt(CtPtrs_VecCt(e),CtPtrs_VecCt(g),CtPtrs_VecCt(tmp),CtPtrs_VecCt(S[j]);
-        Y[i] += e.multiply(enca[j]);      
-      }
-    }*/
+}
 
-    /*for(int i=0;i<5;i++){
-      for(int j=0;j<32;j++){
-        ZZX ptSum;
-        publicKey.decrypt(ptSsum,Y[i][j]);
-        cout<<ptSum[0];
-      }
-      cout<<"....."<<endl;
+// 四位全加器
+void full_adder(std::vector<Ctxt>& val , Ctxt x){
+    
+    for(int i=0;i<4;i++){
+        Ctxt t = x;
+        x.multiplyBy(val[i]);
+        val[i] += t;
+
     }
-  }*/
+
 
 }
 
@@ -206,18 +387,17 @@ Ctxt Less(std::vector<Ctxt> m1, std::vector<Ctxt> m2) {
     long length = m1.size();
     for(long i = 0; i < length; i++) {
         Ctxt cLess(m1[0].getPubKey());
-        cLess = m2[i];             //cLess = B
+        cLess = m1[i];             //cLess = B
         cLess.addCtxt(cOne);       //cLess = B+1
-        cLess.multiplyBy(m1[i]);   //CLess = A(B+1)
-        for(long j = i + 1; j < length; j++) {
-            std::vector<Ctxt> sub_m1, sub_m2;
-            for(long k = j; k < length; k++) {
-                sub_m1.push_back(m1[k]);
-                sub_m2.push_back(m2[k]);
-            }
-            cEqual.push_back(Equal2(sub_m1, sub_m2));
+        cLess.multiplyBy(m2[i]);   //CLess = A(B+1)
+        std::vector<Ctxt> sub_m1, sub_m2;
+        sub_m1.clear();
+        sub_m2.clear();
+        for(long j = 0;j<i;j++){
+            sub_m1.push_back(m1[j]);
+            sub_m2.push_back(m2[j]);
         }
-        if(i != length -1) cLess.multiplyBy(divOrAdd(cEqual));
+        if(i != 0)cLess.multiplyBy(Equal2(sub_m1,sub_m2));
         cSum.push_back(cLess);
     }
     return divOrAdd(cSum);
